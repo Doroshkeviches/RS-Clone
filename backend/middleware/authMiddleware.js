18709aff939772aeab0956f4fb62fb76b6c95dc9
Doroshkeviches <Doroshkeviches@yandex.ru>
2023-02-06 06:38:15 +0300

    fix: swich error language to eng

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -7,13 +7,13 @@ if (req.method === 'OPTIONS') {
 try {
     const token = req.headers.authorization.split(' ')[1]
     if(!token) {
-        return res.status(403).json({message: 'Пользователь не авторизован'})
+        return res.status(403).json({message: 'User not authorized'})
     }
     const decodedData = jwt.verify(token,secret)
     req.user = decodedData
     next()
 } catch (e) {
     console.log(e)
-    return res.status(403).json({message: 'Пользователь не авторизован'})
+    return res.status(403).json({message: 'User not authorized'})
 }
 }
--- backend/middleware/roleMiddleware.js
+++ backend/middleware/roleMiddleware.js
@@ -8,7 +8,7 @@ return function (req, res, next){
     try {
         const token = req.headers.authorization.split(' ')[1]
         if(!token) {
-            return res.status(403).json({message: 'Пользователь не авторизован'})
+            return res.status(403).json({message: 'User not authorized'})
         }
         const {roles: userRoles} = jwt.verify(token,secret)
         let hasRole = false
@@ -18,13 +18,13 @@ return function (req, res, next){
             }
         });
         if(!hasRole){
-            return res.status(403).json({message: 'У вас нет доступа'})
+            return res.status(403).json({message: `You don't have access`})
 
         }
         next()
     } catch (e) {
         console.log(e)
-        return res.status(403).json({message: 'Пользователь не авторизован'})
+        return res.status(403).json({message: 'User not authorized'})
     }
 }
 }
