2f2dd13df2fc1041c2883314b46cdb6b8464a048
RetiredHedgehog <lukashov9999@gmail.com>
2023-02-06 15:57:04 +0300

    feat: add eslint and webpack
    refactor: fix spacing

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,27 +1,46 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
+const EslingPlugin = require('eslint-webpack-plugin');
+
 module.exports = {
-    mode: 'development',
-    entry : './src/registrationPage/scripts/index.js',
-    output : {
+  mode: 'development',
+  entry : './src/index.ts',
+  output : {
     filename: '[contenthash].js',
     path: path.resolve(__dirname,'dist'),
-    clean : true
-},
-plugins : [
- new HTMLWebpackPlugin({
-    template: './src/registrationPage/registrationPage.html'
- }),
-],
-module: {
-    rules : [
-        {test: /\.css/,
-        use: ['style-loader','css-loader'],
-    },
-    {
-        test: /\.s[ac]ss$/i,
-        use: ["style-loader","css-loader","sass-loader",],
+    clean : true,
+  },
+  plugins : [
+    new HTMLWebpackPlugin(
+      {
+        title: "RS-Clone",
       },
-    ]
-}
+    ),
+    new EslingPlugin(
+      {
+        extensions: 'ts',
+        fix: true,
+      }
+    ),
+  ],
+  module: {
+    rules : [
+      {
+        test: /\.css/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
 }
