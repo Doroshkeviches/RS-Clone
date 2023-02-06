faa91bb297a5c647698b9739e8b216dc2fb396ad
RetiredHedgehog <lukashov9999@gmail.com>
2023-02-06 16:10:36 +0300

    feat: add mini-css-extract-plugin

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,16 +1,12 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const EslingPlugin = require('eslint-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry : './src/index.ts',
-  output : {
-    filename: '[contenthash].js',
-    path: path.resolve(__dirname,'dist'),
-    clean : true,
-  },
-  plugins : [
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  const plugins = [
     new HTMLWebpackPlugin(
       {
         title: "RS-Clone",
@@ -22,25 +18,49 @@ module.exports = {
         fix: true,
       }
     ),
-  ],
-  module: {
-    rules : [
-      {
-        test: /\.css/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
+  ];
+
+  if (isProduction) {
+    plugins.push(
+      new MiniCSSExtractPlugin(
+        {
+          filename:'style.[contenthash].css',
+        },
+      )
+    );
+  }
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry : './src/index.ts',
+    output : {
+      filename : isProduction ? 'index.[contenthash].js' : 'index.js',
+      path: path.resolve(__dirname,'dist'),
+      clean : true,
+    },
+    plugins,
+    module: {
+      rules : [
+        {
+          test: /\.css/,
+          use: [
+            isProduction ? MiniCSSExtractPlugin.loader : 'style-loader',
+            'css-loader',
+          ],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.ts$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+  };
 }
