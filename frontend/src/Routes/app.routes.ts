6eb3002b4c46d956cff08bf6e40f3fbd80103711
Artem sssss <inanmate631@gmail.com>
2023-02-11 21:39:22 +0200

    fix: delete test routing template

--- frontend/src/Routes/app.routes.ts
+++ frontend/src/Routes/app.routes.ts
@@ -1,10 +1,8 @@
 import { routesObj } from './Routes.interface';
-import { testTemplate } from '../Pages/testRouting/testTemplate';
 import { autorizationTemplate } from '../Pages/registrationPage/registration.template';
 
 //TODO add all pages path and component
 export const appRoutes: routesObj[] = [
   { path: '', component: autorizationTemplate },
   { path: 'autorization', component: autorizationTemplate },
-  { path: 'test', component: testTemplate },
 ];
