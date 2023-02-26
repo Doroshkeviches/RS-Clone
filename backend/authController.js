f56fdd3acb7082c6d9c0a5f1902da2390006ee4b
Artem sssss <inanmate631@gmail.com>
2023-02-26 15:49:03 +0200

    fix: fix require

--- backend/authController.js
+++ backend/authController.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+const fetch = require('node-fetch');
 const User = require('./models/User');
 const Role = require('./models/Role');
 const bcrypt = require('bcryptjs');
